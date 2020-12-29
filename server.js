f7f03c3b981863394f5bf019c85833139a10ebfd
Adam <adamlevitz@gmail.com>
2020-12-29 12:20:07 -0800

    clean up code

--- server.js
+++ server.js
@@ -18,132 +18,112 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-nunjucks.configure("views",{
+nunjucks.configure("views", {
   autoescape: true,
   express: app
 });
 
 app.get("/", function (req, res, next) {
-  try{
-    // let data = {message: 'nunjucks!!!!'}
-    // return res.render('index.html', { title: data.message, sub: 'Using nunjucks' });
+  try {   
     return res.render('index.html');
-  } catch(err) {
+  } catch (err) {
     return next(err);
   }
 });
 
 
-app.post("/", async function(req, res, next){
-  try{
+app.post("/", async function (req, res, next) {
+  try {
     const query = req.body.movieList;
-  const url = apiBaseTemplate + "&s=" + query;
+    const url = apiBaseTemplate + "&s=" + query;
 
-   const response = await axios.get(url);
+    const response = await axios.get(url);
 
-   movieSearchResults = response.data.Search;
+    movieSearchResults = response.data.Search;
 
-   res.redirect("/results");
-  } catch(err) {
+    res.redirect("/results");
+  } catch (err) {
     return next(err);
-
-  }  
+  }
 });
 
 app.get("/results", async function (req, res, next) {
-  try{
-    // console.log(movieSearchResults);
-  // console.log(random);  
-  // let data = {message: "hello there!"};
-  return res.render('results.html', { movies: movieSearchResults });
-  // res.send(movieSearchResults);
-} catch(err) {
-  return next(err);
-
-} 
+  try {
+    return res.render('results.html', { movies: movieSearchResults });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 app.get("/results/:Title", async function (req, res, next) {
-  try{
+  try {
     const requestedTitle = req.params.Title;
-  const url = apiBaseTemplate + "&t=" + requestedTitle;
+    const url = apiBaseTemplate + "&t=" + requestedTitle;
 
-  movieData = await axios.get(url);
+    movieData = await axios.get(url);
 
-  let titleResult, thumbsUp, thumbsDown;
+    let titleResult, thumbsUp, thumbsDown;
 
-  const result = await db.promise().query(
+    const result = await db.promise().query(
     `SELECT movie_title, release_year, thumbs_up, thumbs_down
     FROM movie
     WHERE movie_title="${movieData.data.Title}"
     AND release_year="${movieData.data.Year}"`);
-    
-     titleResult = result[0];
-    if(titleResult.length){
+
+    titleResult = result[0];
+    if (titleResult.length) {
       thumbsUp = result[0][0].thumbs_up;
       thumbsDown = result[0][0].thumbs_down;
     } else {
       thumbsUp = 0;
       thumbsDown = 0;
-    } 
+    }
 
-  return res.render('movieDetails.html', { movieData, thumbsUp, thumbsDown });
-} catch(err) {
-
-
-  return next(err);
-} 
-  
+    return res.render('movieDetails.html', { movieData, thumbsUp, thumbsDown });
+  } catch (err) {
+    return next(err);
+  }
 });
 
-app.post("/updatevote", async function(req, res, next){
-  const {movie_title, release_year, thumbs_up, thumbs_down} = req.body;
-  console.log(movie_title, release_year, thumbs_up, thumbs_down);
-
-  try{
+app.post("/updatevote", async function (req, res, next) {
+  const { movie_title, release_year, thumbs_up, thumbs_down } = req.body;
+  
+  try {
     const result = await db.promise().query(
     `SELECT movie_title, release_year
     FROM movie
     WHERE movie_title="${movie_title}"
     AND release_year="${release_year}"`);
-    
-     titleResult = result[0];
-    if(titleResult.length){
+
+    titleResult = result[0];
+    if (titleResult.length) {
       await db.promise().query(
-      `UPDATE movie
+     `UPDATE movie
       SET ${thumbs_up || thumbs_down} = ${thumbs_up || thumbs_down} + 1
       WHERE movie_title = "${movie_title}"
-      AND release_year="${release_year}"`  
+      AND release_year="${release_year}"`
       );
 
-  res.send(`movie already in db, updated ${thumbs_up || thumbs_down} up!`);
+      res.send(`movie already in db, updated ${thumbs_up || thumbs_down} up!`);
 
-      } else {
-        await db.promise().query(`INSERT INTO MOVIE (movie_title, release_year) VALUES('${movie_title}', '${+release_year}')`);
-        await db.promise().query(
+    } else {
+      await db.promise().query(`INSERT INTO MOVIE (movie_title, release_year) VALUES('${movie_title}', '${+release_year}')`);
+      await db.promise().query(
         `UPDATE movie
         SET ${thumbs_up || thumbs_down} = ${thumbs_up || thumbs_down} + 1
         WHERE movie_title = "${movie_title}"
-        AND release_year="${release_year}"`  
-        );
-        res.send(`movie added to db, updated ${thumbs_up || thumbs_down} up!`);
-      }
-     } catch(err){
-        console.log(err);
-      }
-
+        AND release_year="${release_year}"`
+      );
+      res.send(`movie added to db, updated ${thumbs_up || thumbs_down} up!`);
+    }
+  } catch (err) {
+    return next(err);
+  }
 });
-
-// app.post("/", function(req, res){
-//   console.log(req.body);
-//   let one = 'working!!!!';
-//   res.send(one);
-// });
-
 
 /** 404 handler */
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error("Not Found");
   err.status = 404;
 
